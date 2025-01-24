b9b758b9edbdd253d9b95febec31427ea8351fb5
NNR8608220 <n8reyes2006@gmail.com>
2025-01-24 15:03:59 -0600

    Finalized Lab

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,7 +1,43 @@
 package org.example;
+import java.util.Scanner;
 
 public class App {
-  public static void main(String[] args) {
-    System.out.println("You can create a driver program for the converter here!");
+
+  public static void main(String[] args) 
+  {
+    Converter ozCvt = new Converter();
+    boolean isValid = false;
+    int ounces = 0;
+    System.out.println("Welcome to The Weight Converter!");
+    System.out.println("Here, we convert your ounces into pounds");
+    System.out.println();
+
+    while (isValid == false)
+    {
+      System.out.println("Please Insert Oz:");
+      Scanner in = new Scanner(System.in);
+      if(in.hasNextInt())
+      {
+        ounces = in.nextInt();
+        isValid = true;
+        System.out.println();
+        System.out.println("Generating Pounds");
+
+      } else {
+        System.out.println();
+        System.out.println("Error: Ounce amount is not an Integer");
+        
+      }
+
+      System.out.println();
+      
+    }
+
+    System.out.println(ounces + " is equal to " + ozCvt.toPounds(ounces) + ".");
+    System.out.println(ounces + " is equal to " + ozCvt.toPoundsAndOunces(ounces) + ".");
+    System.out.println();
+    System.out.println("Thank you for using The Weight Converter!");
+
   }
+
 }
--- app/src/main/java/org/example/Converter.java
+++ app/src/main/java/org/example/Converter.java
@@ -2,4 +2,22 @@ package org.example;
 
 public class Converter {
   // Your code goes here
+  public Converter()
+  {
+
+  }
+
+  public String toPounds(int ounces)
+  {
+    double pounds = ounces / 16.0;
+    String str = String.format("%.4f", pounds);
+    return str + " lbs";
+  }
+
+  public String toPoundsAndOunces(int ounces)
+  {
+    int pounds = ounces / 16;
+    int remainder = ounces % 16;
+    return pounds + " lbs " + remainder + " oz";
+  }
 }
